38574d88a8419e1f41dacc6eaf5a54aaa76532c7
Lit-to <swordworld0327@gmail.com>
2024-11-24 01:17:54 +0900

    名前にスペースが含まれている人の対応

--- bot/dbot.py
+++ bot/dbot.py
@@ -459,7 +459,9 @@ async def notice(interaction: discord.Interaction):
     num_list=bingo.bingo(1,100)
     await interaction.response.defer(thinking=True)
     # await interaction.followup.send(str(num_list).replace(" ",""))
-    await interaction.followup.send("[ビンゴカードを開く](https://lit-to.github.io/tadabot/index.html?card="+str(num_list)+"&?name="+interaction.user.display_name+")")
+    user_name=interaction.user.display_name
+    user_name=user_name.replace(" ","")
+    await interaction.followup.send("[ビンゴカードを開く](https://lit-to.github.io/tadabot/index.html?card="+str(num_list)+"&?name="+user_name+")")
     await interaction.followup.send(file=discord.File(os.path.join("work.jpg")))
 
 @tree.command(name='exit', description='ばいばーい')
