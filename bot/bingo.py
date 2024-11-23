859a24a5b383ee5e1ce3760fa1cc56d95078f579
Lit-to <swordworld0327@gmail.com>
2024-11-23 23:01:37 +0900

    decode関数がないことに気が付いた

--- bot/bingo.py
+++ bot/bingo.py
@@ -40,6 +40,12 @@ def runLength(s=str or list) -> str: #ランレングス圧縮
     return "".join(result)
 
 
+
+def decode(key):
+    alp = "abcdefghijklmnopqrstuvwxyz"
+    return (alp[key[0]]) * 26 + alp[key[1]]
+
+
 def card(numlist):
     print(numlist[:24])
     return compression(numlist[:24])
