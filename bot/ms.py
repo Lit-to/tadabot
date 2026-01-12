dea7a44ffed4a7b77afced1a755e525d5f45342d
Lit-to <swordworld0327@gmail.com>
2026-01-12 23:41:58 +0900

    サーバー起動コマンドを追加

--- bot/ms.py
+++ bot/ms.py
@@ -1,12 +1,16 @@
-import subprocess, sys
+import subprocess
 
 
 def getMsInfo(key, server):
     subprocess.Popen(["D:\\ssn\\start.bat"])
-    config = open("msConfig.txt", "r").readlines()
-    if key == "True":
-        print("\n".join(config))
-    else:
-        print(config[0])
+    config = open("msConfig.txt", "r", encoding="utf-8").readlines()
+    result = []
     if server == "True":
         subprocess.Popen(["E:\\minecraft\\server\\tkp2026\\start.bat"])
+        result.append("サーバーを起動中です！少々お待ちください。")
+    if key == "True":
+        result.append(config[0])
+        result.append(config[1])
+    else:
+        result.append(config[0])
+    return "\n".join(result)
