9316a33db297942104ff3ace2f2d782f3dbf3f07
Lit-to <swordworld0327@gmail.com>
2026-01-12 23:55:16 +0900

    メッセージの形を変更

--- bot/dbot.py
+++ bot/dbot.py
@@ -477,10 +477,10 @@ async def notice(
     interaction: discord.Interaction, key: str = "False", server: str = "False"
 ):
     fo.printf(interaction.user.name, 'did "/ms":', key, server)
-    await interaction.response.send_message(
+    await interaction.response.send_message(ms.getMsInfo(key, server), ephemeral=True)
+    await interaction.followup.send(
         "サーバーを起動中です！少々お待ちください。", ephemeral=False
     )
-    await interaction.response.send_message(ms.getMsInfo(key, server), ephemeral=True)
 
 
 @tree.command(name="67", description="そんなぁ！")
